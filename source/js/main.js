d028702f9cf7155c7a77e991286af81cf8d3f545
gxyz <18710890823@163.com>
2016-12-23 21:33:44 +0800

    add post list

--- source/js/main.js
+++ source/js/main.js
@@ -19,4 +19,17 @@ $(document).ready(function() {
         $("html,body").animate({ scrollTop: 0 }, 800);//点击go to top按钮时，以800的速度回到顶部，这里的800可以根据你的需求修改
         return false;
     });
+
+    $("#show-post-list").click(function () {
+        if ($("#post-list").css("right") !== "0px") {
+            $("#post-list").animate({right:'0px'});
+            $("body").animate({paddingRight:'320px'});
+            $("#show-post-list").text("隐藏文章目录");
+        } else {
+            $("#post-list").animate({right:'-320px'});
+            $("body").animate({paddingRight:'0px'});
+            $("#show-post-list").text("显示文章目录");
+        }
+    })
+    
 })
